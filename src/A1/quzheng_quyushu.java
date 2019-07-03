dde184bca8d12530fd734160e0e760402e7f088a
1023368308@qq.com <ganmou>
2019-07-03 21:56:58 +0800

    21：56

---
+++ src/A1/quzheng_quyushu.java
@@ -0,0 +1,38 @@
+package A1;
+
+public class quzheng_quyushu {
+public static void main(String[] args) {
+	System.out.println(4/2);//等于2.0
+	System.out.println(4/3);//等于1.3333所以不能整除
+	
+	
+	
+	//辨别c++与++b的区别
+	//	int c=0;
+//	int b=0;
+//	System.out.println(c++);
+//	System.out.println(++b);
+//	System.out.println(c);
+//	System.out.println(b);
+//	System.out.println("");
+	
+	//
+	 int max = 10;
+	 int num = 5;
+	 boolean b1 = max>44;//boolean只会返回 true/false 
+	 					//条件成立则返回true 反之返回false
+	 boolean b2 = (num%2==1);//5%2有余数 所以是1  所以成立
+	 System.out.println(b1);
+	 System.out.println(b2);
+	 int a3 = 5, b3 = 9, c3 = 5;
+	 System.out.println(a3>=c3);
+	 System.out.println(a3<b3);
+	 System.out.println(c3<a3);
+	
+	 /*一个数的for循环
+	  * for(int a = 0;a<10;a++) {
+		System.out.println(a);
+	}*/
+	
+}
+}
