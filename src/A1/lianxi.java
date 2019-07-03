401a8e521d253897b2cac2131f60fe8ab8507b29
1023368308@qq.com <ganmou>
2019-07-03 08:55:02 +0800

    7

---
+++ src/A1/lianxi.java
@@ -0,0 +1,23 @@
+package A1;
+
+import java.util.Scanner;
+
+public class lianxi {
+	public static void main(String[] args) {
+		
+		
+		int a = 11;
+		int b = 2;
+		int c = c = a/b;
+		System.out.println(c+"%");
+		
+
+//		System.out.println("来两个数相除");
+//		Scanner b = new Scanner(System.in);
+//		Scanner a = new Scanner(System.in);
+//		int c = b+a;
+//		System.out.println(c);
+
+	}
+
+}
